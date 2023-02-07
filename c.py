cf12afb34cc535d74d798786faa7fa544baba6bb
nidhichandra20 <70096220+nidhichandra20@users.noreply.github.com>
2023-02-07 14:54:19 +0530

    Update c.py

--- c.py
+++ c.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-#import seaborn as sns
-#import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import precision_score, recall_score 
